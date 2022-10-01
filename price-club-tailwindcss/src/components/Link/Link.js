e730173c1ccc2d3955d3f005bf3f0f801dc66803
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-01 16:22:18 +0600

    navbar responsive

--- price-club-tailwindcss/src/components/Link/Link.js
+++ price-club-tailwindcss/src/components/Link/Link.js
@@ -3,7 +3,7 @@ import React from "react";
 const Link = ({ route }) => {
   return (
     <div>
-      <li className="px-6 my-2 bg-blue-600 text-white rounded-tl-lg rounded-br-lg hover:bg-amber-400 p-4">
+      <li className="px-6 my-2 bg-blue-600 text-white rounded-tl-lg rounded-br-lg hover:bg-amber-400 p-4 text-center mx-4 sm:mx-10 md:mx-6 lg:mx-2">
         <a href={route.path}>{route.name}</a>
       </li>
     </div>
