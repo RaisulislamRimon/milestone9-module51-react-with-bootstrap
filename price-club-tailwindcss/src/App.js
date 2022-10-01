6aaa370b418cacda31f4aa5829abacf8b97052fd
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-01 18:00:07 +0600

    update

--- price-club-tailwindcss/src/App.js
+++ price-club-tailwindcss/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Edit from "./components/Edit/Edit";
 import Navbar from "./components/Navbar/Navbar";
+import Pricing from "./components/Pricing/Pricing";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Pricing />
       <Edit />
     </div>
   );
---
+++ price-club-tailwindcss/src/components/Pricing/Pricing.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Pricing = () => {
+  return (
+    <div>
+      <h2 className="text-5xl font-bold bg-indigo-300 text-white p-12 text-center">
+        Best Deal of The Town
+      </h2>
+    </div>
+  );
+};
+
+export default Pricing;
