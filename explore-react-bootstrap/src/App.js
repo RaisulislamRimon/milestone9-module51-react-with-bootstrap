aba273d241b66c38e9a6820454cdf68da2e4ba19
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-10-01 02:55:55 +0000

    button added

--- explore-react-bootstrap/src/App.js
+++ explore-react-bootstrap/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button>Simple Button</button>
+      <Button>React Bootstrap Button</Button>
+      <Button variant='danger'>React Bootstrap Button</Button>
     </div>
   );
 }
