7dee37c0e2f70d7301af87f3b3be3850b13680af
MD. Raisul Islam Rimon <rimonppip0@gmail.com>
2022-10-01 15:53:00 +0600

    51-3 Setup tailwind css with Create React App

---
+++ price-club-tailwindcss/src/components/Navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div>
+            <div>
+                <h1 className='text-4xl'>Navbar</h1>
+                <p className='text-xl text-blue-900 bg-yellow-200 w-50 mx-20 mt-4 p-4 rounded-lg text-red'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium, laudantium commodi maxime cupiditate temporibus porro impedit, ad magni ex rerum, dolores sequi amet earum voluptates perspiciatis labore maiores. Deserunt, molestias.</p>
+            </div>
+            <div>
+                <div class="h-14 bg-gradient-to-r from-cyan-500 to-blue-500"></div>
+                <div class="h-14 bg-gradient-to-r from-sky-500 to-indigo-500"></div>
+                <div class="h-14 bg-gradient-to-r from-violet-500 to-fuchsia-500"></div>
+                <div class="h-14 bg-gradient-to-r from-purple-500 to-pink-500"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
